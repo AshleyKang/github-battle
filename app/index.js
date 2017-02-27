485835ff93ada0b352c43937151d59856102cfdd
DESKTOP-3MNI6L5\JIHYE KANG <jihye.ashley.kang@gmail.com>
2017-02-27 21:37:24 +1300

    Pure Functions, Props, and Nesting React Components in React
    Link: https://online.reacttraining.com/courses/50507/lectures/760335#/questions/1

--- app/index.js
+++ app/index.js
@@ -1,12 +1,54 @@
+var USER_DATA = {
+	name: 'Jihye Kang',
+	username: 'AshleyKang',
+	image: 'https://avatars2.githubusercontent.com/u/7676152?v=3'
+}
+
+
 var React = require('react')
 var ReactDOM = require('react-dom')
-var HelloWorld = React.createClass({
-	render: function(){
+
+
+var ProfilePic = React.createClass({
+	render: function() {
+		return (
+			<img src={this.props.imageUrl}/>
+			)
+	}
+});
+
+var ProfileLink = React.createClass({
+	render: function() {
 		return (
 			<div>
-			Hello World!
+			<a href={'https://github.com/' + this.props.username}>
+			{this.props.username}
+			</a>
 			</div>
 			)
 	}
 });
-ReactDOM.render(<HelloWorld />, document.getElementById('app'));
+
+var ProfileName = React.createClass({
+	render: function() {
+		return (
+			<div>
+			{this.props.name}
+			</div>
+			)
+	}
+});
+
+
+var Avatar = React.createClass({
+	render: function(){
+		return (
+			<div>
+			<ProfilePic imageUrl = {this.props.user.image}/>
+			<ProfileName name = {this.props.user.name}/>
+			<ProfileLink username = {this.props.user.username}/>
+			</div>
+			)
+	}
+});
+ReactDOM.render(<Avatar user={USER_DATA} />, document.getElementById('app'));
